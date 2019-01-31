ba4d57f6fe249860c4df35a358a8b79b8dc6d6f5
Sean Beck <sean@Seans-MacBook-Pro.local>
2019-01-31 16:38:40 -0500

    Adding pagination.

--- gallery.js
+++ gallery.js
@@ -1,38 +1,45 @@
 class Gallery {
   constructor(selector = '#gallery', settings = {}) {
-    this.slides = document.querySelectorAll(`${selector} li`);
+    this.slides = document.querySelectorAll(`${selector} li.gallery-slide`);
+    this.paginationButtons = document.querySelectorAll('li.pagination-control');
     this.selector = selector;
     this.settings = settings;
 
     this.slideArray = [].slice.call(this.slides);
-    this.currentSlide = 0;
-    this.slideWidth = document.querySelector(`${selector} li`).offsetWidth;
-    this.slideHeight = document.querySelector(`${selector} img`).offsetHeight;
-    this.galleryWidth = this.slideWidth * (this.slides.length - 1);
+    this.currentSlide = 1;
+    this.slideWidth = document.querySelector(`${selector} li.gallery-slide`).offsetWidth;
   }
 
   init() {
-    this.updatePosition();
-    document.getElementById('next').addEventListener('click', () => {
-        this.nextSlide();
-    });
-
-    document.getElementById('prev').addEventListener('click', () => {
-        this.previousSlide();
-    });
+    this.previousSlide();
+    this.createControls();
   }
 
   nextSlide() {
     this.slideArray.push(this.slideArray.shift());
-    this.updatePosition();
     this.currentSlide++;
+    this.updatePosition();
     return this.currentSlide;
   }
 
   previousSlide() {
     this.slideArray.unshift(this.slideArray.pop());
-    this.updatePosition();
     this.currentSlide--;
+    this.updatePosition();
+    return this.currentSlide;
+  }
+
+  goToSlide(slideIndex) {
+    let delta = this.currentSlide - slideIndex;
+
+    if(delta < 0) {
+      for(let i = 0; i > delta; i--)
+        this.nextSlide();
+    } else if(delta > 0) {
+      for(let i = 0; i < delta; i++)
+        this.previousSlide();
+    }
+
     return this.currentSlide;
   }
 
@@ -42,6 +49,23 @@ class Gallery {
     for(let slide of this.slideArray) {
       slide.style.transform = "translate(" + (i * this.slideWidth) + "px, 0)";
       i++;
+    }
+  }
+
+  createControls() {
+    document.getElementById('next').addEventListener('click', () => {
+        this.nextSlide();
+    });
+
+    document.getElementById('prev').addEventListener('click', () => {
+        this.previousSlide();
+    });
+
+    for(let pager of this.paginationButtons) {
+      pager.addEventListener('click', (event) => {
+        let index = event.target.dataset.index;
+        this.goToSlide(index);
+      });
     }
   }
 }
